ede2851c75c05c37a7fea5b00557b428d83af2d9
Sveklov Danila <sveklov.danila@bk.ru>
2024-04-03 00:27:06 +0300

    Add .gitignore

--- events/telegram/messages.go
+++ events/telegram/messages.go
@@ -1,6 +1,5 @@
 package telegram
 
-// вместо кавычек пишем ` ` чтобы сохранять форматирование
 const msgHelp = `I can save and keep you pages. Aslo i can offer u them to read.
 In order to save page, just send me a link to it with.
 
@@ -10,8 +9,8 @@ Caution! After that, this page will be removed from your list!`
 const msgHello = "Hi there! \n\n" + msgHelp
 
 const (
-	msgUnknownCommand = "Unknown command🤔"
-	msgNoSavedPages   = "You haven't saved pages😔"
-	msgSaved          = "Saved!👌"
-	msgAlreadyExist   = "You already have this page in your list😴"
+	msgUnknownCommand = "Unknown command 😳"
+	msgNoSavedPages   = "You haven't saved pages 😭"
+	msgSaved          = "Saved! 👌"
+	msgAlreadyExist   = "You already have this page in your list 😴"
 )
